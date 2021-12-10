dd6e505de9e91b42c0b4ecea789ce74a8174e3fa
Tinshea <Malekbouzarkouna58@gmail.com>
2021-12-10 19:50:03 +0100

    index

--- Commande.java
+++ Commande.java
@@ -3,17 +3,28 @@ public class Commande {
     private int choix_menu;
     public static final Commande C = new Commande();
     ArrayList<Menu> menu = new ArrayList<Menu>(); 
+    Menu m;
+   
 
-    public void setchoix_menu(int choix_menu){
-        this.choix_menu=choix_menu;
-    }
     private Commande(){
-        Menu m=new Menu(choix_menu);
-        menu.add(m);
+       
     }
     public static Commande getInstance(){return C;}    
 
+    public void addMenu(int choix_menu){
+        this.choix_menu=choix_menu;
+        m=new Menu(choix_menu);
+        menu.add(m);}
+
+       public void addlememeMenu(){
+        menu.add(m.clone());}
+
     public String toString(){
-        return "votre"+choix_menu;
+        String s="je repete votre commande, vous avez bien choisie :\n";
+        for(int i=0;i<menu.size();i++){
+            s+=menu.get(i)+"\n";
+        }
+        return s;
+
     }
 }
