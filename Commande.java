cc8a4df6f504e5ad39bea2c5bc9e6a1da0c55cb8
Tinshea <Malekbouzarkouna58@gmail.com>
2021-12-10 16:42:13 +0100

    index

--- Commande.java
+++ Commande.java
@@ -1,15 +1,19 @@
-import javax.management.loading.PrivateClassLoader;
-
 import java.util.ArrayList;
 public class Commande {
-
+    private int choix_menu;
     public static final Commande C = new Commande();
     ArrayList<Menu> menu = new ArrayList<Menu>(); 
 
-    private Commande(){}
+    public void setchoix_menu(int choix_menu){
+        this.choix_menu=choix_menu;
+    }
+    private Commande(){
+        Menu m=new Menu(choix_menu);
+        menu.add(m);
+    }
     public static Commande getInstance(){return C;}    
 
     public String toString(){
-        return "marche";
+        return "votre"+choix_menu;
     }
 }
